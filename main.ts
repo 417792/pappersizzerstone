6d9d30c129f9747986c9a20e8070ab10600e2358
417792 <115142684+417792@users.noreply.github.com>
2023-03-16 15:33:34 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,56 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # # . . #
-        # # . # .
-        . . # . .
-        # # . # .
-        # # . . #
-        `)
-    x = 13
+radio.onReceivedNumber(function (receivedNumber) {
+    yourturn = receivedNumber
+    while (myturn == 0) {
+        basic.showString("?")
+    }
+    basic.pause(1000)
+    win_or_lose()
+    basic.pause(3000)
+    設定出值()
 })
+function win_or_lose () {
+    if (myturn == yourturn) {
+        basic.showIcon(IconNames.EigthNote)
+    } else if (myturn == 3 && yourturn == 9) {
+        basic.showIcon(IconNames.Yes)
+    } else if (myturn == 6 && yourturn == 3) {
+        basic.showIcon(IconNames.Yes)
+    } else if (myturn == 9 && yourturn == 6) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    if (myturn == 0) {
+        basic.showIcon(IconNames.Scissors)
+        myturn = 6
+        radio.sendNumber(myturn)
+    }
+})
+function 設定出值 () {
+    myturn = 0
+    yourturn = 0
+    basic.showArrow(ArrowNames.East)
+}
 input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    x = 8
+    if (myturn == 0) {
+        basic.showIcon(IconNames.Square)
+        myturn = 3
+        radio.sendNumber(myturn)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # # # .
-        . # # # .
-        . . . . .
-        `)
-    x = 21
+    if (myturn == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+        myturn = 9
+        radio.sendNumber(myturn)
+    }
 })
-let x = 0
+let myturn = 0
+let yourturn = 0
 radio.setGroup(254)
+設定出值()
 basic.forever(function () {
 	
 })
